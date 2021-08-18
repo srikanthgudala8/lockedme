e0d052f9e46dff3acf3c2afcf698613bdf354c86
Srikanth Gudala <srikanthgudala8@gmail.com>
2021-08-19 02:14:51 +0530

    all files added

---
+++ src/com/lockedmeproject/FileManager.java
@@ -0,0 +1,103 @@
+package com.lockedmeproject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager 
+{
+	/**
+	 * This method will return the file names from the folder
+	 * @param folderpath
+	 * @return List<String>
+	 */
+	public static List<String> getAllFiles(String folderpath)
+	{
+		
+		//Creating File Object
+		File filel= new File(folderpath);
+		
+		// Getting all the files into file array
+		File[] listOfFiles = filel.listFiles();
+		
+		//Declare a list to store a file names
+		List<String> fileNames = new ArrayList<String>();
+		
+		for(File fl:listOfFiles)
+			fileNames.add(fl.getName());
+		
+		//return the list
+		return fileNames;
+		 
+	}
+	
+	/**
+	 * This method will create or append into file specified
+	 * @param folderpath
+	 * @param Foldername
+	 * @return boolean
+	 */
+	
+	public static boolean writeContentToFiles(String folderpath,String fileName,List<String> content) {
+	
+		try {
+			File file1=new File(folderpath,fileName);
+			FileWriter fw=new FileWriter(file1);
+			
+			for(String s:content)
+			{
+				fw.write(s+"\n");
+			}
+			fw.close();
+			return true;
+			
+		}
+		catch(Exception Ex) {
+			return false;
+		}
+		
+	}
+	/**
+	 * This method will delete the file if it exists 
+	 * @param folderpath
+	 * @param fileName
+	 * @return
+	 */
+	
+	public static boolean deleteFile(String folderpath,String fileName) 
+	{
+		File file = new File(folderpath+"\\"+fileName);
+		
+		try {
+			if(file.delete())
+				return true;
+			else
+				return false;
+		}
+		catch(Exception Ex) {
+			return false;
+		}
+		
+	}
+	
+	
+	/**
+	 * This method will search the file from the folder
+	 * @param folderpath
+	 * @param fileName
+	 * @return boolean
+	 */
+	
+	public static boolean searchFile(String folderpath,String fileName) 
+	{
+		File file = new File(folderpath+"\\"+fileName);
+		
+		if(file.exists()) {
+			return true;
+		}
+		else
+			return false;
+		
+	}
+ }
---
+++ src/com/lockedmeproject/LockedMeClass.java
@@ -0,0 +1,163 @@
+package com.lockedmeproject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LockedMeClass {
+	
+	static final String folderpath="G:\\Lockedme project\\Files";
+	public static void main(String[] args) {
+		
+		int proceed=1;
+			
+		
+		do {
+				//Variable declaration
+				int ch;
+				
+				//Display Menu
+				ch=displayMenu();
+				
+				
+				switch(ch) {
+				case 1: getAllFiles();
+						break;
+				case 2: writeContentToFiles();
+						break;
+				case 3:deleteFile();
+						break;
+				case 4:searchFile();
+						break;
+				case 5:System.exit(0);
+						break;
+				default:System.out.println("invalid Selection");
+						break;
+				}
+				
+		}
+			while(proceed>0);
+	}
+	
+	public static int displayMenu() {
+		Scanner obj= new Scanner(System.in);
+		int ch;
+		System.out.println();
+		System.out.println(" ******************************************************************");
+		System.out.println("\t\t\t Lockedme.com");
+		System.out.println(" ******************************************************************");
+		System.out.println(" 1. Display all files");
+		System.out.println(" 2. Create a new file");
+		System.out.println(" 3. Delete the existing file");
+		System.out.println(" 4. Search the file");
+		System.out.println(" 5. Exit");
+		System.out.println(" ******************************************************************");
+		System.out.println(" Please enter your choice:");
+		ch=Integer.parseInt(obj.nextLine());
+		
+		return ch;
+	}
+	
+	/**
+	 * This method will display all the files in the folder.
+	 */
+	
+	public static void getAllFiles() {
+		
+	List<String>fileNames =FileManager.getAllFiles(folderpath);
+	
+		if(fileNames.size()==0) {
+			System.out.println("No files in the directory");
+		}
+		else {
+			System.out.println("File list is below \n");
+			for(String f:fileNames)
+				System.out.println(f);
+		}
+		
+	}
+	
+	/**
+	 * This method will create new files.
+	 */
+	
+	public static void writeContentToFiles()
+	{
+		//Declaring the variable
+		Scanner obj= new Scanner(System.in);
+		String fileName;
+		int linesCount;
+		List<String> content = new ArrayList<String>();
+		
+		//Read file names from user
+		System.out.println("Enter file Name:");
+		fileName=obj.nextLine();
+		
+		//Read number of line from user
+		System.out.println("Enter how many lines in the file:");
+		linesCount=Integer.parseInt(obj.nextLine());
+		
+		//Read lines from user
+		for(int i=1;i<=linesCount;i++)
+		{
+			System.out.println("Enter line"+i+":");
+			content.add(obj.nextLine());
+		}
+		
+		//Save the content into the file
+		boolean isSaved=FileManager.writeContentToFiles(folderpath, fileName, content);
+		
+		if(isSaved)
+			System.out.println("File and Data are saved successfully");
+		else
+			System.out.println("Some error occured");		
+	}
+	
+	/**
+	 * This method will delete the files from the folder.
+	 */
+	public static void deleteFile() {
+		
+		String fileName;
+		Scanner obj =new Scanner(System.in);
+		System.out.println("Enter the file which you have to delete:");
+		fileName= obj.nextLine();
+		
+		boolean isDelete=FileManager.deleteFile(folderpath, fileName);
+		
+		if(isDelete) {
+			System.out.println("File deleted successfully");
+		}
+		else {
+			System.out.println("Either file not there or some access issue");
+		}
+
+	}
+	
+	
+	/**
+	 * This method will search the files to the user.
+	 */
+	public static void searchFile() {
+		
+
+				String fileName;
+				Scanner obj =new Scanner(System.in);
+				System.out.println("Enter the file name to be searched:");
+				fileName= obj.nextLine();
+				
+				boolean isFound=FileManager.searchFile(folderpath, fileName);
+				
+				if(isFound) {
+					System.out.println("File is available in the folder");
+				}
+				else {
+					System.out.println("File not available in the folder");
+				}
+				
+
+	}
+}
+
+
+
